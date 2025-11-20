ec5504e77b1f4e209c50e230848a4466dfa93d62
mahmuuudtolba <mahmuuudtolba@gmail.com>
2025-11-20 15:48:08 +0200

    send batch embedding at once

--- app/infrastructure/llm/openai_embedding.py
+++ app/infrastructure/llm/openai_embedding.py
@@ -1,6 +1,6 @@
 from app.domain.entities.embedding import Embedding
 from typing import List
-from openai import OpenAI
+from openai import AsyncOpenAI
 from app.application.interfaces.embedding_service import IEmbeddingService
 from app.domain.exceptions import EmbeddingError
 class OpenAIEmbeddingService(IEmbeddingService):
@@ -8,11 +8,11 @@ class OpenAIEmbeddingService(IEmbeddingService):
 
     def __init__(self , model_name:str = "text-embedding-3-small"):
         self.model_name = model_name
-        self.client = OpenAI()
+        self.client = AsyncOpenAI()
         
 
     async def create_embedding(self, text: str) -> Embedding:
-        response = self.client.embeddings.create(
+        response = await self.client.embeddings.create(
             input= text , 
             model = self.model_name
         )
@@ -25,30 +25,21 @@ class OpenAIEmbeddingService(IEmbeddingService):
 
     async def create_embeddings_batch(self ,texts:List[str]) -> List[Embedding]:
         """Create embedding for multiple texts"""
-        response = self.client.embeddings.create(
-            input=texts , 
-            model = self.model_name
-        )
-
         vectors = []
 
-        for text in texts:
-            try:
-                response = self.client.embeddings.create(
-                    input=text ,
+        response = await self.client.embeddings.create(
+                    input=texts ,
                     model=self.model_name
                 )
 
-                vector = response.data[0].embedding    
+        for i , embedding_data in enumerate(response.data):
                 vectors.append(
                     Embedding(
-                        vector=vector,
+                        vector=embedding_data.embedding,
                         model= self.model_name,
-                        text=text
+                        text=texts[i]
                     )
                 ) 
 
-            except Exception :
-                raise EmbeddingError 
 
         return vectors
